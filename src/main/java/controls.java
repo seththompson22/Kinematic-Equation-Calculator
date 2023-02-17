0e3190fb5367a182750b9e4b9232a6f6fe511657
Seth Thompson <thompsonrussell22@gmail.com>
2023-02-17 17:52:43 -0500

    Create Repo

---
+++ src/main/java/controls.java
@@ -0,0 +1,6 @@
+public class controls {
+    public static void main(String[] args) {
+        // main control structure for the application
+        kinematicEquations.displacement();
+    }
+}
---
+++ src/main/java/kinematicEquations.java
@@ -0,0 +1,6 @@
+public class kinematicEquations {
+    public static double displacement() {
+        double x = 0.0; // filler value for now
+        return x;
+    }
+}
