728a5c3fc73010224def9891d2ae9ffdf1bf6469
Seth Thompson <thompsonrussell22@gmail.com>
2023-02-17 22:29:33 -0500

    set up structure for methods

--- src/main/java/controls.java
+++ src/main/java/controls.java
@@ -1,6 +1,16 @@
 public class controls {
     public static void main(String[] args) {
         // main control structure for the application
-        kinematicEquations.displacement();
+        double v_0 = 12.0; // meters/second
+        double v_f = 9.06; // meters/second
+        double displacement = 3.16; // meters
+        double acceleration = -9.8; // meters/second^2 (gravity)
+        double time = 0.3; // seconds
+        int missing = 0; // 0-3 that represents which unit is missing in index of parameter list
+        kinematicEquations.notDisplacement(v_f, v_0, acceleration, time, missing); // finds displacement
+        kinematicEquations.notFinalVelocity(displacement, v_0, time, acceleration, missing);
+        kinematicEquations.notTime(v_f, v_0, acceleration, displacement, missing);
+        kinematicEquations.notAcceleration(displacement, v_0, v_f, time, missing);
+        kinematicEquations.notInitialVelocity(displacement, v_f, time, acceleration, missing);
     }
 }
--- src/main/java/kinematicEquations.java
+++ src/main/java/kinematicEquations.java
@@ -1,6 +1,22 @@
 public class kinematicEquations {
-    public static double displacement() {
-        double x = 0.0; // filler value for now
-        return x;
+    public static double notDisplacement(double finalVelocity, double initialVelocity, double acceleration, double time, int missing) {
+        // filler value for now
+        return finalVelocity;
+    }
+    public static double notFinalVelocity(double displacement, double initialVelocity, double time, double acceleration, int missing) {
+        // filler value for now
+        return displacement;
+    }
+    public static double notTime(double finalVelocity, double initialVelocity, double acceleration, double displacement, int missing) {
+        // filler value for now
+        return finalVelocity;
+    }
+    public static double notAcceleration(double displacement, double initialVelocity, double finalVelocity, double time, int missing) {
+        // filler value for now
+        return displacement;
+    }
+    public static double notInitialVelocity(double displacement, double finalVelocity, double time, double acceleration, int missing) {
+        // filler value for now
+        return displacement;
     }
 }
