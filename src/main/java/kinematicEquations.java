908704a18d9e8df1fcc26e30e99d11655cd7d068
Seth Thompson <thompsonrussell22@gmail.com>
2023-02-17 22:36:40 -0500

    commented explanations

--- src/main/java/kinematicEquations.java
+++ src/main/java/kinematicEquations.java
@@ -1,6 +1,8 @@
 public class kinematicEquations {
     public static double notDisplacement(double finalVelocity, double initialVelocity, double acceleration, double time, int missing) {
-        // filler value for now
+        // filler value for now, there will be 4 calculations per method based on which value is missing
+        // (missing values will be zero and the missing variable will also indicate the variable
+        // that is unknown, in case of zero as a normal input)
         return finalVelocity;
     }
     public static double notFinalVelocity(double displacement, double initialVelocity, double time, double acceleration, int missing) {
